bb4dca8bdadb799118ddfccde01ab10e68f6d5ef
Rosie Gardner <rosiegardner78@gmail.com>
2022-01-27 18:55:46 -0800

    add JS functions

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#surveyForm").submit(function(event) {
+    const name = $("input#name").val();
+    const age = parseInt($("input#age").val());
+    const pronouns = $("select#pronouns").val();
+    const animal = $("select#animal").val();
+    const vehical = $("select#vehical").val();
+    
+    if (animal === 'cats', 'dogs', 'snakes' && vehical === 'vans') {
+      $("#language1-hidden").show();
+    } else if (animal === 'dogs', 'cats', 'snakes' && vehical === 'trucks') {
+      $("#language2-hidden").show();
+    } if (animal === 'snakes', 'dogs', 'cats' && vehical === 'motor') {
+      $("#language3-hidden").show();
+    }
+
+    event.preventDefault();
+  });
+});
