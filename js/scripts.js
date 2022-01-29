4eb05a0285c19bf90ddabd9bc3584cacebe1dc12
Rosie Gardner <rosiegardner78@gmail.com>
2022-01-29 11:31:52 -0800

    BUG FIX: fixing answer results for stacking

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   $("#surveyForm").submit(function(event) {
-    event.preventDefault();
     const name = $("input#name").val();
     const age = parseInt($("input#age").val());
     const pronouns = $("select#pronouns").val();
     const vehical = $("select#vehical").val();
     const animal = $("select#animal").val();
-    $('#surveyForm').trigger("reset");
+    $("surveyForm").trigger("#reset")
 
     
     if (vehical === 'vans', 'trucks', 'motor' && animal === 'snakes') {
       $("#language1-hidden").show();
+      $("#language2-hidden").hide();
+      $("#language3-hidden").hide();
     } else if (vehical === 'vans', 'trucks', 'motor' && animal === 'dogs') {
+      $("#language1-hidden").hide();
       $("#language2-hidden").show();
+      $("#language3-hidden").hide();
     } if (vehical === 'vans', 'trucks', 'motor' && animal === 'cats') {
+      $("#language1-hidden").hide();
+      $("#language2-hidden").hide();
       $("#language3-hidden").show();
     }
+      event.preventDefault();
 
   });
 });
